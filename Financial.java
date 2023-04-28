b5dd69dcb0e8d05d3e6f5c49596e95c37ac557eb
Kseniya Veka <73733574+Ksusha-Veko@users.noreply.github.com>
2023-04-28 15:18:08 +0300

    Delete Financial.java

--- Financial.java
+++
@@ -1,9 +0,0 @@
-package task;
-
-public class Financial {
-    public static String getValueInEuro(long cost) {
-        return String.format("%d.%02d", cost / 100, Math.round(cost % 100));
-    }
-}
-
-
